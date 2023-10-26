0fe7ff77842b8437b4856b69b4b5ae25172adf2d
Kevin7744 <arapbiisubmisions@gmail.com>
2023-10-26 15:42:37 +0300

    Test crawler

--- scaper.py
+++ scaper.py
@@ -1,83 +1,64 @@
 from bs4 import BeautifulSoup
+from urllib.request import urlopen
 import pprint
+import re
+import random
+import datetime
 # with open ("indx.html", "r") as html_file:
 #  readFile = BeautifulSoup(html_file, "html.parser")  
 # pTags = readFile.find_all("p")[0]
 
 # pprint.pprint(pTags.string)
+pp = pprint.PrettyPrinter(depth=6)
 
-def getTitle(file):
-    try:
-        html_file = open (file, "r")
-    except FileNotFoundError as fileError:
-        print("Check file or file not found")
-    try:
-        readFIle = BeautifulSoup(html_file, "html.parser")
-        title = readFIle.title.string
-    except AttributeError as error:
-        return None 
-    return title
+# def getTitle(url):
+#         html = urlopen('https://en/wikipedia.org{}'.format(url))
+#         readFIle = BeautifulSoup(html, "html.parser")
+#         title = readFIle.title.string
+#         return title
 
-def get_P_Tags(file):
-    try:
-        html_file = open (file, "r")
-    except FileNotFoundError as fileError:
-        print("Check file or file not found")
-    try:
-        readFIle = BeautifulSoup(html_file, "html.parser")
-        pTags = readFIle.find_all("p", string="Kevin Kipkoech")
-    except AttributeError as error:
-        print("There are no p tags in the page, or there is just a problem")
-    return pTags
+# def getLinks(file):
+#     links = []
+#     text = []
+#     try:
+#         html_file = open(file, "r")
+#         readFile = BeautifulSoup(html_file, 'html.parser')
+#     except Exception as error:
+#         print("There seems to be a problem somewhere with the file")
+#     for link in readFile.find('div', {'class': 'box'}).find_all('a', {'class': 'itm'}):
+#         if 'href' in link.attrs:
+#             links.append(link.attrs['href'])
+#             for link in links:
+#                 pp = pprint.PrettyPrinter(depth=6)
+#                 pp.pprint(links)
+#     for text in readFile.find('div', {'class': 'box'}).find_all('span', {'class':'text'}):
+#         if 'text' in text.attrs:
+#             text.append(text.attr['text'])
+#             for text in text:
+#                 pp = pprint.PrettyPrinter(depth=6)
+#                 pp.pprint(text)
+#     return (links, text)
 
-def getDivs(file):
-    try:
-        html_file = open (file, "r")
-    except FileNotFoundError as fileError:
-        print("Check file or file not found")
-    try:
-        readFIle = BeautifulSoup(html_file, "html.parser")
-        divs = readFIle.find('div', {"class":"sidebar"}).find_all('div', {"class": "contact"})
-    except AttributeError as error:
-        print("There was problem getting the divs")
-    return divs
-def getTable(file):
-    try:
-        html_file = open(file, "r")
-    except FileNotFoundError as fileError:
-        print("Check the file or file not found")
-    try:
-        readFile = BeautifulSoup(html_file, "html.parser")
-        table = readFile.find('div', {"class": "table-container"}).find_all('tr')
-    except AttributeError as error:
-        print("There was a problem getting the table data")
-    return  table
+# title = getTitle('/wiki/Kevin_Bacon')
+# pp.pprint(title)
 
-
-title = getTitle("Index.html")
-if title == None:
-    print("Title not found, please check the file again")
-else:
-    print(title)
-
-
-pTags = get_P_Tags("Index.html")
-if pTags == None:
-    print("There is a problem getting the p tags")
-else:
-    print(pTags)
-
-
-divs = getDivs("Index.html")
-if divs == None:
-    print("Div not found, Please check prompt again")
-else:
-    print(divs)
-
-table = getTable("Index.html")
-if table == None:
-    print("Table not found, Please check prompt again")
-else:
-    print(table)
-    
+# links = getLinks('jumia.html')
+pages = set()
+# random.seed(datetime.datetime.now())
+def getLinks(articleUrl):
+    html = urlopen('https://en.wikipedia.org{}'.format(articleUrl))
+    bs = BeautifulSoup(html, 'html.parser')
+    for link in bs.find('div', {'id': 'bodyContent'}).find_all('a', href=re.compile('^(/wiki/)')):
+        if 'href' in link.attrs:
+            if link.attrs['href'] not in pages:
+                newPage = link.attrs['href']
+                pp.pprint(newPage)
+                pages.add(newPage)
+                getLinks(newPage)
+getLinks('')
+# links = getLinks('/wiki/Kevin_Bacon')
+# while len(links) > 0:
+#     newArticle = links[random.randint(0, len(links)-1)].attrs['href']
+#     pp.pprint(newArticle)
+#     links = getLinks(newArticle)
 
