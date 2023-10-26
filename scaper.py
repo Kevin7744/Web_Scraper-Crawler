4158669e0aece0e1c1dd3d18c2e0246448da267e
Kevin7744 <arapbiisubmisions@gmail.com>
2023-10-26 22:10:07 +0300

    test crawler

--- scaper.py
+++ scaper.py
@@ -46,12 +46,20 @@ pp = pprint.PrettyPrinter(depth=6)
 pages = set()
 # random.seed(datetime.datetime.now())
 def getLinks(articleUrl):
+    global pages
     html = urlopen('https://en.wikipedia.org{}'.format(articleUrl))
     bs = BeautifulSoup(html, 'html.parser')
+    try:
+        print(bs.h1.get_text())
+        print(bs.find(id = 'mw-content-text').find_all('p')[0])
+        print(bs.find(id = 'ca-edit').find('span').find('a').attrs['href'])
+    except AttributeError:
+        print('This page is missing something, continuing.')
     for link in bs.find('div', {'id': 'bodyContent'}).find_all('a', href=re.compile('^(/wiki/)')):
         if 'href' in link.attrs:
             if link.attrs['href'] not in pages:
                 newPage = link.attrs['href']
+                print('-'*20)
                 pp.pprint(newPage)
                 pages.add(newPage)
                 getLinks(newPage)
